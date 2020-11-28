53b7d74429c58c6c5ca165241cdbe507460b800d
Ismail Guclu <i.guclu@live.nl>
2020-11-28 13:43:46 +0100

    commit crf script

--- code/crf/main.py
+++ code/crf/main.py
@@ -1,20 +1,109 @@
-import sklearn
-import pycrfsuite
+"""
+A baseline CRF model to evaluate the performance of a NER on the 
+i2b2 dataset for de-identification. This file is mainly build 
+upon the tutorial in the documentations: 
+https://sklearn-crfsuite.readthedocs.io/en/latest/tutorial.html
+
+Result: 0.8513459946541471 for feature set with standard tutorial.
+"""
 import os
+import csv
+import sklearn_crfsuite
+import scipy.stats
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn_crfsuite import scorers
+from sklearn_crfsuite import metrics
+from features import doc2features, doc2labels, doc2tokens
+import pickle
+import argparse
 
-# Load data
+# Load data files/path
 TRAIN_PATH = os.getcwd() + "/data/training-df/"
+TEST_PATH = os.getcwd() + "/data/test-gold-df"
+TRAIN = os.listdir(TRAIN_PATH)
+TRAIN.sort()
+TEST = os.listdir(TEST_PATH)
+TEST.sort()
 
-for filename in os.listdir(TRAIN_PATH):
-   with open(os.path.join(TRAIN_PATH, filename), 'r') as f: 
-       
+def get_arguments():
+   parser = argparse.ArgumentParser(description="Train/test CRF model on i2b2 data.")
+   parser.add_argument("--train", help="train/cross-validate model", action="store_true")
+   parser.add_argument("--test", help="test the model", action="store_true")
+   args = parser.parse_args()
+   return args
 
+def get_data(data, data_path):
+   tmp_data, tmp_labels = [], set()
+   for filename in data:
+      tmp_file = []
+      with open(os.path.join(data_path, filename), newline="") as f:
+         reader = csv.reader(f, delimiter="\t")
+         for r in reader:
+            tmp_file.append((r[0], r[1], r[4]))
+            tmp_labels.add(r[4])
+      tmp_data.append(tmp_file)
 
-# if/else for train/dev/test
+   if data_path == TRAIN_PATH:
+      return (tmp_data, tmp_labels)
+   else:
+      return (tmp_data, None)
 
-# add features
+def get_features(data):
+   x = [doc2features(s) for s in data]
+   y = [doc2labels(s) for s in data]
+   return (x, y)
 
-# train crf
-# pred crf
+def train_model(X_train, y_train, k=3, n_iter=50):
+   crf = sklearn_crfsuite.CRF(
+      algorithm='lbfgs',
+      c1=0.1,
+      c2=0.1,
+      max_iterations=100,
+      all_possible_transitions=True
+   )
 
-# evaluate performance
+   params_space = {
+    'c1': scipy.stats.expon(scale=0.5),
+    'c2': scipy.stats.expon(scale=0.05),
+   }
+
+   f1_scorer = make_scorer(metrics.flat_f1_score,
+                           average='weighted', labels=labels)
+
+   rs = RandomizedSearchCV(crf, params_space,
+                           cv=k,
+                           verbose=1,
+                           n_jobs=-1,
+                           n_iter=n_iter,
+                           scoring=f1_scorer)
+   rs.fit(X_train, y_train)
+   crf = rs.best_estimator_
+   print("Best parameters: ", rs.best_params_)
+   print("Best f1-score: ", rs.scorer_)
+
+   with open("final_crf_model.pkl", "wb") as f:
+      pickle.dump(crf, f)
+
+   return crf
+
+def test_model(crf, X_test, y_test, labels):
+   labels.remove("O")
+   y_pred = crf.predict(X_test)
+   print(metrics.flat_f1_score(y_test, y_pred,
+                        average='weighted', labels=labels))
+   return
+
+if __name__ == "__main__":
+   train_data, labels = get_data(TRAIN, TRAIN_PATH)
+   test_data, _ = get_data(TEST, TEST_PATH)
+
+   args = get_arguments()
+
+   if args.train:
+      X_train, y_train = get_features(train_data)
+      crf = train_model(X_train, y_train)
+
+   if args.test:
+      X_test, y_test = get_features(test_data)
+      test_model(crf, X_test, y_test, labels)

6e553b3eb1acdb1f6ca27cc18b5565c977cb449f
ismailguclu <i.guclu@live.nl>
2020-11-30 17:22:52 +0100

    model 3x5 + fix print score

--- code/crf/main.py
+++ code/crf/main.py
@@ -5,6 +5,7 @@ upon the tutorial in the documentations:
 https://sklearn-crfsuite.readthedocs.io/en/latest/tutorial.html
 
 Result: 0.8513459946541471 for feature set with standard tutorial.
+Best parameters:  {'c1': 0.04655479644991304, 'c2': 0.007784780059581591}
 """
 import os
 import csv
@@ -20,7 +21,8 @@ import argparse
 
 # Load data files/path
 TRAIN_PATH = os.getcwd() + "/data/training-df/"
-TEST_PATH = os.getcwd() + "/data/test-gold-df"
+TEST_PATH = os.getcwd() + "/data/test-gold-df/"
+MODEL_PATH = os.getcwd() + "/model/"
 TRAIN = os.listdir(TRAIN_PATH)
 TRAIN.sort()
 TEST = os.listdir(TEST_PATH)
@@ -34,27 +36,22 @@ def get_arguments():
    return args
 
 def get_data(data, data_path):
-   tmp_data, tmp_labels = [], set()
+   tmp_data = []
    for filename in data:
       tmp_file = []
       with open(os.path.join(data_path, filename), newline="") as f:
          reader = csv.reader(f, delimiter="\t")
          for r in reader:
             tmp_file.append((r[0], r[1], r[4]))
-            tmp_labels.add(r[4])
       tmp_data.append(tmp_file)
-
-   if data_path == TRAIN_PATH:
-      return (tmp_data, tmp_labels)
-   else:
-      return (tmp_data, None)
+   return tmp_data
 
 def get_features(data):
    x = [doc2features(s) for s in data]
    y = [doc2labels(s) for s in data]
    return (x, y)
 
-def train_model(X_train, y_train, k=3, n_iter=50):
+def train_model(X_train, y_train, k=3, n_iter=5):
    crf = sklearn_crfsuite.CRF(
       algorithm='lbfgs',
       c1=0.1,
@@ -69,7 +66,7 @@ def train_model(X_train, y_train, k=3, n_iter=50):
    }
 
    f1_scorer = make_scorer(metrics.flat_f1_score,
-                           average='weighted', labels=labels)
+                           average='weighted')
 
    rs = RandomizedSearchCV(crf, params_space,
                            cv=k,
@@ -80,14 +77,15 @@ def train_model(X_train, y_train, k=3, n_iter=50):
    rs.fit(X_train, y_train)
    crf = rs.best_estimator_
    print("Best parameters: ", rs.best_params_)
-   print("Best f1-score: ", rs.scorer_)
+   print("Best f1-score: ", rs.best_score_)
 
-   with open("final_crf_model.pkl", "wb") as f:
+   with open(MODEL_PATH + "/final_crf_model.pkl", "wb") as f:
       pickle.dump(crf, f)
 
    return crf
 
-def test_model(crf, X_test, y_test, labels):
+def test_model(crf, X_test, y_test):
+   labels = list(crf.get_classes_)
    labels.remove("O")
    y_pred = crf.predict(X_test)
    print(metrics.flat_f1_score(y_test, y_pred,
@@ -95,15 +93,18 @@ def test_model(crf, X_test, y_test, labels):
    return
 
 if __name__ == "__main__":
-   train_data, labels = get_data(TRAIN, TRAIN_PATH)
-   test_data, _ = get_data(TEST, TEST_PATH)
-
    args = get_arguments()
 
    if args.train:
+      train_data = get_data(TRAIN, TRAIN_PATH)
       X_train, y_train = get_features(train_data)
       crf = train_model(X_train, y_train)
 
    if args.test:
+      test_data = get_data(TEST, TEST_PATH)
       X_test, y_test = get_features(test_data)
+
+      with open(MODEL_PATH + "final_crf_model.pkl", "rb") as f:
+         crf = pickle.load(f)
+         
       test_model(crf, X_test, y_test, labels)
